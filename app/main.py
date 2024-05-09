120e66e360251642654b692372177a96de3b24e9
Ag <agata.a.rajska@gmail.com>
2024-05-09 15:56:45 +0200

    respond with pong to ping

--- app/main.py
+++ app/main.py
@@ -3,9 +3,12 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
+    pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn = server_socket.accept() # wait for client
+    conn.receive(1024)
+    conn.sendall(pong.encode())
 
 
 if __name__ == "__main__":
