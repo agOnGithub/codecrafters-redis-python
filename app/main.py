9c959a597f946c9fd16b6784cf0b5425a6a8d5a4
Ag <agata.a.rajska@gmail.com>
2024-05-09 16:12:44 +0200

    apparently threading is the wrong way to go

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
 import socket
-from threading import Thread
 
 def main():
     print("Logs from your program will appear here!")
     pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        conn, _ = server_socket.accept() # wait for client
-        Thread(target=return_pong, args=(conn, pong)).start()
-
-def return_pong(Client: socket.socket, pong):
-    Client.recv(1024)
-    Client.send(pong.encode())
+    conn, _ = server_socket.accept() # wait for client
+    
+    while conn.recv(1024):
+        conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
