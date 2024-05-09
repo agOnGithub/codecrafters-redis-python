a8d403261560577b6392ff67f028a46501204e50
Ag <agata.a.rajska@gmail.com>
2024-05-09 16:36:50 +0200

    felt like trying something out, might delete later

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ def main():
 def send_reply(conn: socket.socket):
     pong = "+PONG\r\n"
     cmd = conn.recv(1024).decode()
-    while True:
-        if "ping" in str(cmd.decode("utf-8")).lower():
-            conn.send(pong.encode())
-        else:
-            conn.send(cmd.encode())
+    if cmd.split(" ")[0] == "ping":
+        conn.send(ping.encode())
+    else:
+        conn.send(cmd.encode())
 
 if __name__ == "__main__":
     main()
