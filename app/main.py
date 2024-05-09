c7ffcece70dc26f68e6e2a7ed133c1c35d917c17
Ag <agata.a.rajska@gmail.com>
2024-05-09 16:18:49 +0200

    are threads allowed now?"

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import socket
+from threading import Thread
 
 def main():
     print("Logs from your program will appear here!")
     pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept() # wait for client
     
-    while conn.recv(1024):
-        conn.send(pong.encode())
+    while True:
+        conn, _ = server_socket.accept() # wait for client
+        Thread(target=send_pong, args=(conn, pong)).start()
+        
+        
+ def send_pong(conn: socket.socket, pong):       
+    conn.recv(1024)
+    conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
