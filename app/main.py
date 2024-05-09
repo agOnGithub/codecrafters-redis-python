daf4da0cbe49e94afb815b05fc310589eeeb7482
Ag <agata.a.rajska@gmail.com>
2024-05-09 16:33:10 +0200

    i might be underthinking here but maybe this will work

--- app/main.py
+++ app/main.py
@@ -2,18 +2,21 @@ import socket
 from threading import Thread
 
 def main():
-    print("Logs from your program will appear here!")
-    pong = "+PONG\r\n"
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
     while True:
         conn, _ = server_socket.accept() # wait for client
-        Thread(target=send_pong, args=(conn, pong)).start()
+        Thread(target=send_reply, args=(conn)).start()
 
-def send_pong(conn: socket.socket, pong):
-    while conn.recv(1024):
-        conn.send(pong.encode())
+def send_reply(conn: socket.socket):
+    pong = "+PONG\r\n"
+    
+    while True:
+        cmd = conn.recv(1024)
+        if "ping" in str(cmd.decode("utf-8")).lower():
+            conn.send(pong.encode())
+        else:
+            conn.send(cmd.encode())
 
 if __name__ == "__main__":
     main()
