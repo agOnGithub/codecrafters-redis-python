3fb76e19e981baa8be08ac48d76cedee0a62e330
Ag <agata.a.rajska@gmail.com>
2024-05-09 16:06:03 +0200

    pass pong

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         conn, _ = server_socket.accept() # wait for client
-        Thread(target=return_pong, args=(conn)).start()
+        Thread(target=return_pong, args=(conn, pong)).start()
 
-def return_pong(Client: socket.socket):
+def return_pong(Client: socket.socket, pong):
     conn.recv(1024)
     conn.sendall(pong.encode())
 
