090c048d552ad74256b472a1e94f01db70e8ec8a
Ag <agata.a.rajska@gmail.com>
2024-05-09 15:57:24 +0200

    forgot I''m coding in Python

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn = server_socket.accept() # wait for client
-    conn.receive(1024)
+    conn.recv(1024)
     conn.sendall(pong.encode())
 
 
