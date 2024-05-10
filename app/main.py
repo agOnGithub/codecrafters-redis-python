1cd33bce501032f93c599e06606a960c87a4699b
Ag <agata.a.rajska@gmail.com>
2024-05-10 10:06:40 +0200

    i don't know anymore

--- app/main.py
+++ app/main.py
@@ -3,29 +3,31 @@ from threading import Thread
 
 def send_reply(conn: socket.socket):
     pong = "+PONG\r\n"
-    while True:
-        cmd = conn.recv(1024).decode()
-        data = str(data.decode("utf-8")).strip()
-        parts = data.split("\r\n")
-        command = parts[2].lower()
+    with conn:
+        while True:
+            cmd = conn.recv(1024).decode()
+            data = str(data.decode("utf-8")).strip()
+            parts = data.split("\r\n")
+            command = parts[2].lower()
 
-        if "ping" == command:
-             conn.sendall(pong.encode("utf-8"))
+            if "ping" == command:
+                 conn.sendall(pong.encode("utf-8"))
 
-        elif "echo" == command:
-            raw_message_length = len(parts)
-            message = ""
+            elif "echo" == command:
+                raw_message_length = len(parts)
+                message = ""
 
-            for i in range(4, raw_message_length, 2):
-                message += parts[i]
-                if i < raw_message_length - 1:
-                    message += " "
-                    
-            message_send = f"${len(message)}\r\n{message}\r\n"
-            conn.sendall(message_send.encode("utf-8"))
+                for i in range(4, raw_message_length, 2):
+                    message += parts[i]
+                    if i < raw_message_length - 1:
+                        message += " "
+                        
+                message_send = f"${len(message)}\r\n{message}\r\n"
+                conn.sendall(message_send.encode("utf-8"))
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.listen()
     
     while True:
         conn, _ = server_socket.accept() # wait for client
